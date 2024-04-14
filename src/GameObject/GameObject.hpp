844c212d660034264cf0c3f33a26d8d4fdeec81e
riyorax <maximiliansulistiyo@gmail.com>
2024-04-14 21:36:02 +0700

    fix : changes on Petani and Peternak

--- src/GameObject/GameObject.hpp
+++ src/GameObject/GameObject.hpp
@@ -7,95 +7,141 @@
 
 using namespace std;
 
-class GameObject {
-    protected:
-        int id;
-        string kode_huruf;
-        string nama;
-    
-    public:
+class GameObject
+{
+protected:
+    int id;
+    string kode_huruf;
+    string nama;
 
-        // constructors
-        GameObject(int id, string kode_huruf, string nama){
-            this->id = id;
-            this->kode_huruf = kode_huruf;
-            this->nama = nama;
-        }
-        GameObject() : GameObject(0,"   ","   "){}
-        
-        int getId(){
-            return this->id;
-        }
-        string getKodeHuruf(){
-            return this->kode_huruf;
-        }
+public:
+    // constructors
+    GameObject(int id, string kode_huruf, string nama)
+    {
+        this->id = id;
+        this->kode_huruf = kode_huruf;
+        this->nama = nama;
+    }
+    GameObject() : GameObject(0, "   ", "   ") {}
 
-        string getNamaGameObject(){
-            return this->nama;
-        }
-        virtual void incrementWeight(){};
-        
+    int getId()
+    {
+        return this->id;
+    }
+    string getKodeHuruf()
+    {
+        return this->kode_huruf;
+    }
+
+    string getNamaGameObject()
+    {
+        return this->nama;
+    }
+    virtual void incrementWeight(){};
 };
 
-class TradeObject : public GameObject {
-    private:
-        int price;
-        string type;
+class TradeObject : public GameObject
+{
+private:
+    int price;
+    string type;
 
-    public:
-        TradeObject(int p, string t){
-            price = p;
-            t = type;
-        }
-        void cetakBarang(){
-            cout << " " << kode_huruf <<" |";
-        }
+public:
+    TradeObject(int id, string kode, string nama, int price,
+                string type) : GameObject(id, kode, nama)
+    {
+        price = price;
+        type = type;
+    }
+    TradeObject() : GameObject()
+    {
+        this->price = 0;
+        this->type = "KOSONG";
+    }
+    void cetakBarang()
+    {
+        cout << " " << kode_huruf << " |";
+    }
 
-        int getPrice(){
-            return this->price;
-        }
-        string getType(){
-            return this->type;
-        }
+    int getPrice()
+    {
+        return this->price;
+    }
+    string getType()
+    {
+        return this->type;
+    }
+
+    int virtual getAddedWeight();
 };
 
-class CultivatedObject : public TradeObject {
-    private:
-        int cultivateWeight;
-        int currentWeight;
-    public:
-        void incrementWeight() override{
-            cultivateWeight++;
-        }
-        void addWeight();
+class CultivatedObject : public TradeObject
+{
+private:
+    int cultivateWeight; // berat ready
+    int currentWeight;   // berat skrg
+
+public:
+    void incrementWeight() override
+    {
+        cultivateWeight++;
+    }
+
+    void setWeight(int weight)
+    {
+        this->currentWeight = weight;
+    }
+    void addWeight(int weight)
+    {
+        this->setWeight(weight + this->currentWeight);
+    }
 };
 
-class ProductObject : public TradeObject {
-    private:
-        string origin;
-};
+class ProductObject : public TradeObject
+{
+private:
+    string origin;
+    int addedWeight;
 
+public:
+    ProductObject(int prodId, string prodKode, string nama, int prodPrice,
+                  string prodType, string origin, int addedWeight) : TradeObject(prodId, prodKode, nama, prodPrice, prodType)
+    {
+        this->addedWeight = addedWeight;
+        this->origin = origin;
+    }
+
+    int getAddedWeight()
+    {
+        return this->addedWeight;
+    }
+};
 
 // recipe seharusnya bukan tradeobject, gabisa di trade soalnya
-class Recipe : public GameObject {
-    private :
-        int price;
-        vector<string> listMaterial;
-        vector<int> materialQuantity;
-    public:
-        Recipe() : GameObject(){
-            this->price = 0;
+class Recipe : public GameObject
+{
+private:
+    int price;
+    vector<string> listMaterial;
+    vector<int> materialQuantity;
+
+public:
+    Recipe() : GameObject()
+    {
+        this->price = 0;
+    }
+    Recipe(int id, string kode_huruf, string nama, int price, vector<string> listMaterials, vector<int> materialQuantity) : GameObject(id, kode_huruf, nama)
+    {
+        for (int i = 0; i < listMaterial.size(); i++)
+        {
+            this->listMaterial.push_back(listMaterials[i]);
+            this->materialQuantity.push_back(materialQuantity[i]);
         }
-        Recipe(int id, string kode_huruf, string nama, int price, vector<string>listMaterials, vector<int> materialQuantity) : GameObject(id,kode_huruf,nama){
-            for (int i = 0; i < listMaterial.size() ; i++){
-                this->listMaterial.push_back(listMaterials[i]);
-                this->materialQuantity.push_back(materialQuantity[i]);
-        
-            }
-        }
-        int getPrice(){
-            return this->price;
-        }
+    }
+    int getPrice()
+    {
+        return this->price;
+    }
 };
 
 #endif
--- src/Player/Player.hpp
+++ src/Player/Player.hpp
@@ -1,58 +1,28 @@
-
 #include "../GameObject/gameObject.hpp"
 #include "../FieldObject/Field.hpp"
 
-class Player{
-    protected:
-        int id;
-        int kekayaan;
-        int berat;
-        string nama;
-        string peran;
-        Field<TradeObject> penyimpanan;
-        
-    public:
-        Player(int id, string nama, string peran, int berat){
-            this->id = id;
-            this->nama = nama;
-            this->peran = peran;
-            this->berat = berat;
-        }
-        ~Player() {
-        }
-        void virtual next(){}
-        void virtual muat();
-        void virtual jual(int, int);
-        void virtual beli(TradeObject*, int);
-        void virtual makan();
-        void virtual tambahPlayer(Game& game, int id,string nama, string peran);
-        // throw utk yg lain selain walikota
-        void virtual cetakPenyimpanan(){
-            penyimpanan.cetak();
-        }
+class Player
+{
+protected:
+    int id;
+    int kekayaan;
+    int berat;
+    string nama;
+    string peran;
+    Field<TradeObject> penyimpanan;
 
-        virtual int bayarPajak();
-        void virtual cetakLahan();
-        string getNama(){
-            return this->nama;
-        }
-        string getPeran(){
-            return this->getPeran();
-        }
-        void setKekayaan(int kekayaan){
-            this->kekayaan = kekayaan;
-        }
-        float persentasePajak(int KKP){
-            if(KKP <= 6){
-                return 0.5;
-            }else if(KKP <=25){
-                return 0.15;
-            }else if(KKP <= 50){
-                return 0.25;
-            }else if(KKP <= 500){
-                return 0.3;
-            }else{
-                return 0.35;
-            }
-        }
+public:
+    Player(int id, string nama, string peran, int berat);
+    ~Player();
+    void virtual next() {}
+    void virtual muat();
+    void virtual jual(int, int);
+    void virtual beli(TradeObject *, int);
+    void virtual makan();
+    void virtual cetakPenyimpanan();
+    void virtual cetakLahan();
+    string getNama();
+    string getPeran();
+    void setKekayaan(int kekayaan);
+    float persentasePajak(int KKP);
 };

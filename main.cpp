d5542d3397a1b47ecb55622c02e8f52d0a37a8cc
FrancescoMichael <113195073+FrancescoMichael@users.noreply.github.com>
2024-04-15 09:01:15 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
-#include "FieldObject/Field.hpp"
-#include "GameMaster/GameMaster.hpp"
-#include "GameObject/GameObject.hpp"
-#include "Player/Player.hpp"
+// #include "src/FieldObject/Field.hpp"
+// #include "src/GameMaster/GameMaster.hpp"
+// #include "src/GameObject/GameObject.hpp"
+// #include "src/Player/Player.hpp"
+
+#include <iostream>
+
+using namespace std;
 
 int main() {
-    
+    // clearTerminal();
+    const string ENCAPSULATORS = "▄▄▄▄▄▄▄ ▄▄    ▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄   ▄▄ ▄▄▄     ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄   ▄▄▄▄▄▄▄\n█       █  █  █ █       █       █       █       █  █ █  █   █   █       █       █       █   ▄  █ █       █\n█    ▄▄▄█   █▄█ █       █   ▄   █    ▄  █  ▄▄▄▄▄█  █ █  █   █   █   ▄   █▄     ▄█   ▄   █  █ █ █ █  ▄▄▄▄▄█\n█   █▄▄▄█       █     ▄▄█  █▄█  █   █▄█ █ █▄▄▄▄▄█  █▄█  █   █   █  █▄█  █ █   █ █  █ █  █   █▄▄█▄█ █▄▄▄▄▄ \n█    ▄▄▄█  ▄    █    █  █       █    ▄▄▄█▄▄▄▄▄  █       █   █▄▄▄█       █ █   █ █  █▄█  █    ▄▄  █▄▄▄▄▄  █\n█   █▄▄▄█ █ █   █    █▄▄█   ▄   █   █    ▄▄▄▄▄█ █       █       █   ▄   █ █   █ █       █   █  █ █▄▄▄▄▄█ █\n█▄▄▄▄▄▄▄█▄█  █▄▄█▄▄▄▄▄▄▄█▄▄█ █▄▄█▄▄▄█   █▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█▄▄█ █▄▄█ █▄▄▄█ █▄▄▄▄▄▄▄█▄▄▄█  █▄█▄▄▄▄▄▄▄█\n";
+    cout << ENCAPSULATORS;
+
+    // Game game = new Game();
+
+    cout << "Menu : \n";
+    cout << "1. Tanpa baca berkas\n";
+    cout << "2. Baca berkas\n";
+
+    while(true) {
+        
+    }
 }
